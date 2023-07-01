6c2387d3408d64c1558b35c5455a4b9697b2791e
Johhni <83779358+Johhni@users.noreply.github.com>
2023-07-01 23:16:05 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,28 +1,28 @@
 const characterData = {
     hero: {
         name: "Wizard",
-        avatar: "rpg-game/images/wizard.png",
+        avatar: "images/wizard.png",
         health: 60,
         diceCount: 3,
         currentDiceScore: []
     },
     orc: {
         name: "Orc",
-        avatar: "rpg-game/images/orc.png",
+        avatar: "images/orc.png",
         health: 30,
         diceCount: 1,
         currentDiceScore: []
     },
     demon: {
         name: "Demon",
-        avatar: "rpg-game/images/demon.png",
+        avatar: "images/demon.png",
         health: 25,
         diceCount: 2,
         currentDiceScore: []
     },
     goblin: {
         name: "Goblin",
-        avatar: "rpg-game/images/goblin.png",
+        avatar: "images/goblin.png",
         health: 20,
         diceCount: 3,
         currentDiceScore: []
